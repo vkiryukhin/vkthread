3d42955386c1ca5dd6f6ff16694157bdb6682694
Vadim Kiryukhin <vkiryukhin@gmail.com>
2014-02-22 23:41:12 -0800

    JSONfn was modified to process RegExp and Date objects.

--- vkthread/vkthread.js
+++ vkthread/vkthread.js
@@ -1,9 +1,9 @@
 /**
  * vkThread - javascript plugin to execute javascript function(s) in a thread.  
  * https://github.com/vkiryukhin/vkthread
-  * http://www.eslinstructor.net/vkthread/
+ * http://www.eslinstructor.net/vkthread/
  *
- * @version: 0.55.00 ( February 2014 )
+ * @version: 0.57.00 ( February 2014 )
  *
  * @author: Vadim Kiryukhin ( vkiryukhin @ gmail.com )
  * Copyright (c) 2013 Vadim Kiryukhin 
@@ -17,65 +17,64 @@
 (function(){
 "use strict";
 
+  var JSONfn = {
+
+/* this is a fragment of JSONfn plugin ( https://github.com/vkiryukhin/jsonfn ) */
+
+    stringify:function (obj) {
+      return JSON.stringify(obj, function (key, value) {
+        if (value instanceof Function || typeof value == 'function') {
+          return value.toString();
+        }
+        if (value instanceof RegExp) {
+          return '_PxEgEr_' + value;
+        }
+        return value;
+      });
+    }
+  };
+
+
+  function Vkthread(){
 /* 
- * This piece of code is taken from JSONfn (c) plugin 
- * https://github.com/vkiryukhin/jsonfn
+ * Set a path to the "worker.js" file, which should be located in the same 
+ * folder with vkthread.js (this one.) To find out the path ee throw "Error" 
+ * object and then parse it ( path is a part of the object.)
+ * User also can provide the path directly with vkthread.setPath() method. 
  */
-var JSONfn = {};
-
-JSONfn.stringify = function (obj) {
-
-  return JSON.stringify(obj, function (key, value) {
-    if (value instanceof Function || typeof value == 'function') {
-      return value.toString();
+    var err;
+    try { 
+      throw new Error();
+    } 
+    catch(e){ 
+      err = e.stack;
     }
-    if (value instanceof RegExp) {
-      return '_PxEgEr_' + value;
+    
+    if (err === 'undefined') {
+      this.path = '';
+    } else {
+      this.path = 'http'+ err.split('http')[1].split('vkthread.js').slice(0,-1) + 'worker.js';
     }
-    return value;
-  });
-};
-/* end JSONfn code */
+  }
 
-function Vkthread(){
+  function _buildObj(obj, fn, args, context, importFiles){
 
-	/* 
-	 * Set a path to the "worker.js" file, which should be located in the same 
-	 * folder with vkthread.js (this one). To find out the path We throw "Error" 
-	 * object and then parse it ( path is a part of the object.)
-	 * User also can provide the path directly with vkthread.setPath() method. 
-	 */
-	var err;
-	try { 
-		throw new Error();
-	} 
-	catch(e){ 
-		err = e.stack;
-	}
-	
-	if (err === 'undefined') {
-		this.path = '';
-	} else {
-		this.path = 'http'+ err.split('http')[1].split('vkthread.js').slice(0,-1) + 'worker.js';
-	}
-}
+    if(Array.isArray(context)) {
+      // the 4-th argument exist, but it is Array, which means 
+      // that this is a list of imported files.
+      obj.imprt = context;
+    } else 
+    if(context) {
+      obj.cntx = context;
+    }
+    
+    if(importFiles) {
+      obj.imprt = importFiles;
+    }
+  }
 
-function _buildObj(obj, fn, args, context, importFiles){
-
-	if(Array.isArray(context)) {//'context' object is not provided.
-		obj.imprt = context;
-	} else 
-	if(context) {
-		obj.cntx = context;
-	}
-	
-	if(importFiles) {
-		obj.imprt = importFiles;
-	}
-}
-
-/*
- * Callback-style API function ( no dependency on libraries )
+/**
+ *   vkthread.exec() - Callback-style API function ( no dependency )
  *
  *   Execute function in the thread and process result in callback function.
  *
@@ -86,99 +85,97 @@ function _buildObj(obj, fn, args, context, importFiles){
  *    @importFiles - Array of Strings;  list of files (with path), which @fn depends on. 
  */
  
- Vkthread.prototype.exec = function(fn, args, cb, context, importFiles){
+   Vkthread.prototype.exec = function(fn, args, cb, context, importFiles){
 
- /* no dependency  */
- 
-	var worker = new Worker(this.path),
-		obj = {fn:fn, args:args, cntx:false, imprt:false};
-		
-	_buildObj(obj, fn, args, context, importFiles);
-	
-	worker.onmessage = function (oEvent) {
-		cb(oEvent.data);
-		worker.terminate();
-	};
-	
-	worker.onerror = function(error) {
-    cb(null, error.message);
-    worker.terminate();
+    var worker = new Worker(this.path),
+      obj = {fn:fn, args:args, cntx:false, imprt:false};
+      
+    _buildObj(obj, fn, args, context, importFiles);
+    
+    worker.onmessage = function (oEvent) {
+      cb(oEvent.data);
+      worker.terminate();
+    };
+    
+    worker.onerror = function(error) {
+      cb(null, error.message);
+      worker.terminate();
+    };
+    
+    worker.postMessage(JSONfn.stringify(obj));
   };
-	
-	worker.postMessage(JSONfn.stringify(obj));
-};
 
-/*
- *   Promise-style API function ( depends on jQuery )
- * 
+
+/**
+ *   vkthread.run() - Promise-style API function ( jQuery-dependent )
+ *
  *   Execute a single function in a thread. 
  *
  *   @fn          - Function;  function to execute in a thread;
  *   @args        - Array;     array of arguments for @fn;
- *    @context     - Object;    object which will be 'this' for @fn.
+ *   @context     - Object;    object which will be 'this' for @fn.
  *   @importFiles - Array of Strings;  list of files (with path), which @fn depends on. 
  */
- Vkthread.prototype.run = function(fn, args,  context, importFiles){
+  Vkthread.prototype.run = function(fn, args,  context, importFiles){
 
- /* depends on jQuery  */
- 
-	var dfr = $.Deferred(),
-		worker = new Worker(this.path),
-		obj = {fn:fn, args:args, cntx:false, imprt:false};
-	
-	_buildObj(obj, fn, args, context, importFiles);
-	
-	worker.onmessage = function (oEvent) {
-		dfr.resolve(oEvent.data);
-		worker.terminate();
-	};
-	
-	worker.onerror = function(error) {
-    dfr.reject(new Error('Worker error: ' + error.message));
-    worker.terminate();
+    var dfr = $.Deferred(),
+      worker = new Worker(this.path),
+      obj = {fn:fn, args:args, cntx:false, imprt:false};
+    
+    _buildObj(obj, fn, args, context, importFiles);
+    
+    worker.onmessage = function (oEvent) {
+      dfr.resolve(oEvent.data);
+      worker.terminate();
+    };
+    
+    worker.onerror = function(error) {
+      dfr.reject(new Error('Worker error: ' + error.message));
+      worker.terminate();
+    };
+    
+    worker.postMessage(JSONfn.stringify(obj));
+    
+    return dfr;
   };
-	
-	worker.postMessage(JSONfn.stringify(obj));
-	
-	return dfr;
-};
 
-/*
- * Promise-style API function ( depends on jQuery )
- * 
+  
+/**
+ * vkthread.runAll() - Promise-style API function ( jQuery-dependent )
+ *
  * Execute multiple functions in a thread(s).
  *
  *   @args - Array of Arrays;  In fact, vkthread.runAll() executes multiple vkthread.run(). 
  *           So, we need to provide arguments for each of them. Each element of the array is 
  *           array of arguments for correspondent vkthread.run() function.  
  */
-Vkthread.prototype.runAll = function(args){
+ 
+  Vkthread.prototype.runAll = function(args){
 
-/* depends on jQuery  */
+    var dfrs = [],
+      len = args.length,
+      ix; 
+    
+    for(ix=0; ix<len; ix++){
+      dfrs.push( this.run.apply(this,args[ix]));
+    }
 
-	var dfrs = [],
-		len = args.length,
-		ix; 
-	
-	for(ix=0; ix<len; ix++){
-		dfrs.push( this.run.apply(this,args[ix]));
-	}
-	
-	return $.when.apply($,dfrs).then(
-				function(){
-					return Array.prototype.slice.call(arguments);
-				}
-			);
-};
+    return $.when.apply($,dfrs).then(
+          function(){
+            return Array.prototype.slice.call(arguments);
+          }
+        );
+  };
 
-/* 
- * API function to set a path to worker.js if needed. It overwrites default setting.
+/** 
+ * vkthread.setPath() - API utility to explicitly set path to worker.js. 
  */
-Vkthread.prototype.setPath = function(path){ 
-	this.path = path;
-};
+ 
+  Vkthread.prototype.setPath = function(path){ 
+    this.path = path;
+  };
 
-window.vkthread = new Vkthread();
+  window.vkthread = new Vkthread();
 
 }());
  
--- vkthread/worker.js
+++ vkthread/worker.js
@@ -10,53 +10,30 @@
 (function() {
 "use strict";
 
-var JSONfn = {};
-
-JSONfn.parse = function (str, date2obj) {
-
-  var iso8061 = date2obj ? /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/ : false;
-
-  return JSON.parse(str, function (key, value) {
-    var prefix;
-
-    if (typeof value != 'string') {
-      return value;
+  var JSONfn = {
+    parse:function (str, date2obj) {
+      var iso8061 = date2obj ? /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/ : false;
+      return JSON.parse(str, function (key, value) {
+        if (typeof value != 'string') return value;
+        if (value.length < 8) return value;
+        if (iso8061 && value.match(iso8061)) return new Date(value);
+        if (value.substring(0, 8) === 'function') return eval('(' + value + ')');
+        if (value.substring(0, 8) === '_PxEgEr_') return eval(value.slice(8));
+        return value;
+      });
     }
+  };
+
+  onmessage = function(e) {
+
+    var obj  = JSONfn.parse(e.data, true),
+        cntx = obj.cntx ? obj.cntx : self;
     
-    if (value.length < 8) {
-      return value;
+    if(obj.imprt){
+      importScripts.apply(null,obj.imprt);
     }
-
-    prefix = value.substring(0, 8);
-
-    if (iso8061 && value.match(iso8061)) {
-      return new Date(value);
-    }
-    
-    if (prefix === 'function') {
-      return eval('(' + value + ')');
-    }
-    
-    if (prefix === '_PxEgEr_') {
-      return eval(value.slice(8));
-    }
-
-    return value;
-  });
-};
-  
-
-onmessage = function(e) {
-
-  var obj  = JSONfn.parse(e.data, true),
-      cntx = obj.cntx ? obj.cntx : self;
-  
-  if(obj.imprt){
-    importScripts.apply(null,obj.imprt);
-  }
-  
-  postMessage(obj.fn.apply(cntx,obj.args));
-};
+    postMessage(obj.fn.apply(cntx,obj.args));
+  };
 
 }());
 
